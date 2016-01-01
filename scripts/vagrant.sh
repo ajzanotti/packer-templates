c3874b9546c66351f57207c76bb6532897f592b8
Alan Zanotti <me@alanzanotti.com>
2015-12-31 19:58:30 -0500

    Centralize provisioning scripts

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -11,6 +11,20 @@
 ###################################################################
 
 VAGRANT_HOME="/home/vagrant"
+DISTRO_VERSION="unknown"
+
+GetDistroVersion()
+{
+  distroVersionString=$(awk '{print $(NF-1);}' /etc/redhat-release)
+  regex='([0-9])\.([0-9]+)'
+
+  if [[ "${distroVersionString}" =~ "${regex}" ]]
+  then
+    DISTRO_VERSION="${BASH_REMATCH[1]}"
+  fi
+}
+
+GetDistroVersion
 
 echo "vagrant" | passwd --stdin root
 
@@ -25,12 +39,16 @@ chmod 0600 $VAGRANT_HOME/.ssh/authorized_keys
 chown -R vagrant:vagrant $VAGRANT_HOME/.ssh
 
 # Allow vagrant to execute all commands without a password
-mkdir /etc/sudoers.d
-chmod 750 /etc/sudoers.d
+if [ "${DISTRO_VERSION}" == "5" ]
+then
+  mkdir /etc/sudoers.d
+  chmod 750 /etc/sudoers.d
+  echo -e "## Read drop-in files from /etc/sudoers.d (the # here does not mean a comment)\n#includedir /etc/sudoers.d" >> /etc/sudoers
+fi
+
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/vagrant
 echo "Defaults:vagrant !requiretty" >> /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
-echo -e "## Read drop-in files from /etc/sudoers.d (the # here does not mean a comment)\n#includedir /etc/sudoers.d" >> /etc/sudoers
 
 # Improve SSH performance by disabling DNS
 sed -i 's/^#UseDNS\ yes/UseDNS\ no/' /etc/ssh/sshd_config
