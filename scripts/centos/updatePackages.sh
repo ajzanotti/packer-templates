54b4f0ac32c2ea0093f50fe639a5d0101b1675f7
Alan Zanotti <azanotti@hmc.psu.edu>
2016-12-13 17:57:47 -0500

    Reorganize scripts directory
    The previous commit attempted to use one updatePackages.sh script
    for both CentOS and Ubuntu VMs. It works, but opens the door to
    code duplication throughout scripts/.
    
    For example, anywhere a package install or update is needed, the
    same check for yum vs. apt-get would have to be performed. Similar
    situations are possible, and though there aren't many scripts now
    there very well could be in the future and it's best to eliminate
    the problem now before it expands.
    
    The possibility for a scripts/generic/ directory exists in the future.
    Scripts that are exactly the same between distributions or scripts
    that could be refactored to be the same could go there to further
    reduce code duplication.

--- scripts/centos/updatePackages.sh
+++ scripts/centos/updatePackages.sh
@@ -10,23 +10,8 @@
 #
 ###################################################################
 
-UPDATE_COMMAND=''
-UPDATE_OPTS=''
-
 # Exit immediately on non-zero status
 set -e
 
-if command -v yum > /dev/null 2>&1
-then
-  UPDATE_COMMAND='yum'
-  UPDATE_OPTS='update -y -q'
-elif command -v apt-get > /dev/null 2>&1
-then
-  UPDATE_COMMAND='apt-get'
-  UPDATE_OPTS='upgrade -qq'
-else
-  exit -1
-fi
-
-$UPDATE_COMMAND $UPDATE_OPTS
+yum update -y -q
 reboot
