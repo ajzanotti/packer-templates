a0de104c7af5963436a08f0b88788a94a942eda3
Alan Zanotti <azanotti@hmc.psu.edu>
2016-12-13 00:07:21 -0500

    Refactor updatePackages.sh to support CentOS and Ubuntu

--- scripts/updatePackages.sh
+++ scripts/updatePackages.sh
@@ -10,8 +10,23 @@
 #
 ###################################################################
 
+UPDATE_COMMAND=''
+UPDATE_OPTS=''
+
 # Exit immediately on non-zero status
 set -e
 
-yum update -y -q
+if command -v yum > /dev/null 2>&1
+then
+  UPDATE_COMMAND='yum'
+  UPDATE_OPTS='update -y -q'
+elif command -v apt-get > /dev/null 2>&1
+then
+  UPDATE_COMMAND='apt-get'
+  UPDATE_OPTS='upgrade -qq'
+else
+  exit -1
+fi
+
+$UPDATE_COMMAND $UPDATE_OPTS
 reboot
