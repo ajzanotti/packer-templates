6bbb7092c5f21c4651ac269dda48889cf701b878
Alan Zanotti <alan.j.zanotti@gmail.com>
2016-12-30 13:50:31 -0500

    Improve amazon-import security
    Cloud-init will block SSH as root, so while it's not strictly necessary
    to set a random root password it's better than the default 'toor'
    password.
    
    SELinux should be in enforcing mode, especially for cloud-hosted
    servers. Currently, the kickstart process starts SELinux in permissive
    mode, which may be a legacy artifact that's no longer necessary. It will
    be reviewed separately.

--- scripts/centos/amazonImport.sh
+++ scripts/centos/amazonImport.sh
@@ -16,4 +16,11 @@ set -e
 
 yum install -y -q cloud-init
 
+# Set random root password
+randomRootPassword="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+echo root:"$randomRootPassword" | chpasswd
+
+# Put SELinux in enforcing mode
+sed -i 's/^SELINUX=permissive/SELINUX=enforcing/' /etc/selinux/config
+
 exit 0
